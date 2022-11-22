41ee67a14a5c78932fcae29df29f9bc8ab8597ac
Moussa Mikhail <moussamikhail56@gmail.com>
2022-11-21 20:54:42 -0600

    Initial commit.

---
+++ src/cardealer/LuxuryBuyer.java
@@ -0,0 +1,28 @@
+package cardealer;
+
+/**
+ * @author Moussa
+ */
+public class LuxuryBuyer extends Buyer {
+
+    private static final Models MODELS = Models.luxuryModels();
+
+    public LuxuryBuyer(String name, CarInfo wantedCar) {
+        super(name, wantedCar);
+    }
+
+    public static LuxuryBuyer generateRandomBuyer() {
+
+        String randomName = Name.getRandomName();
+
+        CarInfo randCar = CarInfo.generateRandomCarInfo(MODELS);
+
+        return new LuxuryBuyer(randomName, randCar);
+    }
+
+    @Override
+    public String toString() {
+        return "LuxuryBuyer{}";
+    }
+}
+
---
+++ src/cardealer/LuxuryCarDealer.java
@@ -0,0 +1,35 @@
+package cardealer;
+
+/**
+ * @author Moussa
+ */
+public final class LuxuryCarDealer extends CarDealer {
+
+    private static final int WARRANTY_PRICE = 5000;
+    private static final Models LUXURY_MODELS = Models.luxuryModels();
+    private static final int MIN_PRICE_IN_THOUSANDS = 80;
+    private static final int PRICE_RANGE_IN_THOUSANDS = 60;
+
+    public LuxuryCarDealer() {
+
+        populateInventory(INITIAL_NUM_CARS, LUXURY_MODELS);
+
+        setPrices(MIN_PRICE_IN_THOUSANDS, PRICE_RANGE_IN_THOUSANDS);
+    }
+
+    @Override
+    public String toString() {
+        return "LuxuryCarDealer{} " + super.toString();
+    }
+
+    @Override
+    public void sellWarranty(Buyer buyer) {
+
+        sellWarranty(buyer, WARRANTY_PRICE);
+    }
+
+    @Override
+    public int getWarrantyPrice() {
+        return WARRANTY_PRICE;
+    }
+}
