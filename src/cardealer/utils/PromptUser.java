5631d64d302406b7cf396f8a56e3fbdf3ac2ab68
Moussa Mikhail <moussamikhail56@gmail.com>
2022-12-01 21:22:32 -0600

    add exception handling.

--- src/cardealer/utils/PromptUser.java
+++ src/cardealer/utils/PromptUser.java
@@ -25,19 +25,47 @@ public final class PromptUser {
         for (int i = 0; i < options.length; i++) {
             System.out.printf("%d. %s%n", i + 1, options[i]);
         }
-        System.out.print("Enter your choice: ");
-        int choice = nextInt();
-        while (choice < 1 || choice > options.length) {
-            System.out.println("Invalid choice. Please enter a valid number.");
-            choice = nextInt();
-        }
+
+        int minChoice = 1;
+        int maxChoice = options.length;
+        int choice = 0;
+        do {
+            System.out.print("Enter your choice: ");
+            try {
+                choice = nextBoundedInt(minChoice, maxChoice);
+            } catch (InvalidInputException e) {
+                System.out.println(e.getMessage());
+            }
+        } while (isNotInBounds(choice, minChoice, maxChoice));
         return options[choice - 1];
     }
 
-    public static int nextInt() {
-        // Necessary because Scanner.nextInt() does not read newline
-        // which breaks the subsequent nextLine() call.
+    public static int nextBoundedInt(int min, int max) throws InvalidInputException {
+        int choice = nextInt();
+        if (isNotInBounds(choice, min, max)) {
+            throw new ChoiceOutOfBoundsException(String.format("Please enter an integer between %d and %d, inclusive.", min, max));
+        }
+        return choice;
+    }
+
+    private static boolean isNotInBounds(int choice, int minChoice, int maxChoice) {
+        return choice < minChoice || choice > maxChoice;
+    }
+
+    public static int nextInt() throws InvalidInputException {
+        // This is necessary because Scanner.nextInt() does not read newline
+        // which will break the subsequent nextLine() call.
         // See https://stackoverflow.com/a/13102045/18650633
-        return Integer.parseInt(Main.CONSOLE.nextLine());
+        String input = Main.CONSOLE.nextLine();
+
+        if (input.isEmpty()) {
+            throw new EmptyInputException("Input is empty. Please enter an integer.");
+        }
+
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new InvalidInputException("Input could not be parsed as an integer. Please enter a valid integer.", e);
+        }
     }
 }
