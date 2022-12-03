ffdefbc979865d65ee9b88da249d517a0065862d
Moussa Mikhail <moussamikhail56@gmail.com>
2022-12-03 14:58:28 -0600

    format.

--- src/main/java/cardealer/exceptions/MultipleCarOptionsRemainingException.java
+++ src/main/java/cardealer/exceptions/MultipleCarOptionsRemainingException.java
@@ -3,7 +3,7 @@ package cardealer.exceptions;
 /**
  * @author Moussa
  */
-public class MultipleCarOptionsRemainingException extends CarOptionsException{
+public class MultipleCarOptionsRemainingException extends CarOptionsException {
     public MultipleCarOptionsRemainingException(String message) {
         super(message);
     }
