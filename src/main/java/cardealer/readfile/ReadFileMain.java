6474030be9c4a0a0d892d9914b9ffb7409773e4b
Moussa Mikhail <moussamikhail56@gmail.com>
2022-12-16 08:39:07 -0600

    handle sentences and use StringUtils.

--- src/main/java/cardealer/readfile/ReadFileMain.java
+++ src/main/java/cardealer/readfile/ReadFileMain.java
@@ -1,49 +1,61 @@
 package cardealer.readfile;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 /**
  * @author Moussa
  */
 public class ReadFileMain {
-    public static final String RESULT_FORMAT = "Number of unique words:";
+    public static final String RESULT_PREFIX = "Number of unique words:";
     private static final String FILENAME = "words.txt";
     private static final Logger LOGGER = LogManager.getLogger(ReadFileMain.class);
 
     public static void main(String[] args) {
         File file = new File(FILENAME);
 
-        List<String> words = null;
+        List<String> lines = null;
         try {
-            words = FileUtils.readLines(file, StandardCharsets.UTF_8);
+            lines = FileUtils.readLines(file, StandardCharsets.UTF_8);
         } catch (IOException e) {
             LOGGER.fatal(e.getMessage());
             System.out.println(e.getMessage());
             System.exit(1);
         }
 
-        String lastLine = words.get(words.size() - 1);
-        if (lastLine.startsWith(RESULT_FORMAT)) {
-            words.remove(words.size() - 1);
+        String lastLine = lines.get(lines.size() - 1);
+        if (lastLine.startsWith(RESULT_PREFIX)) {
+            lines.remove(lines.size() - 1);
         }
 
-        Set<String> uniqueWords = new HashSet<>(words);
-        String result = String.format("Number of unique words: %d.", uniqueWords.size());
+        List<String> words = new ArrayList<>();
+        for (String line : lines) {
+            String[] split = StringUtils.split(line);
+            // Remove punctuation.
+            Arrays.stream(split).map(s -> StringUtils.replaceChars(s, ",.", "")).forEach(words::add);
+        }
 
-        words.add(result);
+        System.out.println("Number of words: " + words.size());
+
+        int numUniqueWords = new HashSet<>(words).size();
+        String result = String.format(RESULT_PREFIX + " %d", numUniqueWords);
+        System.out.println(result);
+
+        lines.add(result);
 
         file = new File(FILENAME);
         try {
-            FileUtils.writeLines(file, words);
+            FileUtils.writeLines(file, lines);
         } catch (IOException e) {
             LOGGER.fatal(e.getMessage());
             System.out.println(e.getMessage());
