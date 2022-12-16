a46e27bd875258b4cb2f6b5068c70a30d4c20fe8
Moussa Mikhail <moussamikhail56@gmail.com>
2022-12-15 19:18:12 -0600

    Initial commit.

---
+++ src/main/java/cardealer/readfile/ReadFileMain.java
@@ -0,0 +1,52 @@
+package cardealer.readfile;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author Moussa
+ */
+public class ReadFileMain {
+    private static final String FILENAME = "words.txt";
+    private static final Logger LOGGER = LogManager.getLogger(ReadFileMain.class);
+
+    public static void main(String[] args) {
+        File file = new File(FILENAME);
+
+        List<String> words = null;
+        try {
+            words = FileUtils.readLines(file, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            LOGGER.fatal(e.getMessage());
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+
+        String lastLine = words.get(words.size() - 1);
+        if (lastLine.startsWith("Number of unique words:")) {
+            words.remove(words.size() - 1);
+        }
+
+        Set<String> uniqueWords = new HashSet<>(words);
+        String message = String.format("Number of unique words: %d.", uniqueWords.size());
+
+        words.add(message);
+
+        file = new File(FILENAME);
+        try {
+            FileUtils.writeLines(file, words);
+        } catch (IOException e) {
+            LOGGER.fatal(e.getMessage());
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+    }
+}
